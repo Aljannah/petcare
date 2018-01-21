fcbe820f8182f87d34870b84f7b78ee7056024ea
latifah72 <latifahrme@gmail.com>
2018-01-21 15:28:47 +0700

    update footer

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,30 +1,92 @@
 
 <!--/start-footer-section-->
 			<div class="footer-section" style="padding-bottom: 0px;">
+               <h3 align="center">Cara Pesan</h3>
+
 			   <div class="container">
+                   <br><br><br>
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <figure>
+                           <img src="<?php echo base_url("icon/if_notepad_1291737.png")?>" width="100px" height="100px">
+                           <figcaption>Lakukan registrasi akun</figcaption>
+                       </figure>
+                   </div> <!-- End of .footer_logo -->
+
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <center>
+                           <figure>
+                               <img src="<?php echo base_url("icon/choices.png")?>" width="100px" height="100px">
+                               <figcaption>Pilih jenis hewan dan paket yang diinginkan</figcaption>
+                           </figure>
+                       </center>
+                   </div> <!-- End of .footer_logo -->
+
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <center>
+                           <figure>
+                               <img src="<?php echo base_url("icon/if_calendar_1291765.png")?>" width="100px" height="100px">
+                               <figcaption>Tentukan tanggal untuk dilakukan grooming</figcaption>
+                           </figure>
+                       </center>
+                   </div> <!-- End of .footer_logo -->
+
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <center>
+                           <figure>
+                               <img src="<?php echo base_url("icon/if_map_1291745.png")?>" width="100px" height="100px">
+                               <figcaption>Cantumkan alamat jelas</figcaption>
+                           </figure>
+                       </center>
+                   </div> <!-- End of .footer_logo -->
+
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <center>
+                           <figure>
+                               <img src="<?php echo base_url("icon/if_waiting_room_41994.png")?>" width="100px" height="100px">
+                               <figcaption>Tunggu konfirmasi dari Penyedia Jasa Grooming</figcaption>
+                           </figure>
+                       </center>
+                   </div> <!-- End of .footer_logo -->
+
+                   <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+                       <center>
+                           <figure>
+                               <img src="<?php echo base_url("icon/if_buy-bill-money-cash-pay_2205221.png")?>" width="100px" height="100px">
+                               <figcaption>Lakukan pembayaran ditempat</figcaption>
+                           </figure>
+                       </center>
+                   </div> <!-- End of .footer_logo -->
+
+
+                   <br><br><br><br><br><br><br><br>
+
+                    <hr>
 					<div class="footer-grids">
-						<div class="col-lg-8 col-md-6 col-sm-6 col-xs-12 footer_logo" >
+
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 footer_logo" >
 								<center>
 									<a href="index.php"><img src="<?php echo base_url();?>assets/images/petcare.png" alt="Logo" ></a>
 									<h4><p>Menyayangi hewan peliharaan sepenuh hati.</p></h4>
 								</center>
-							</div> <!-- End of .footer_logo -->
-					<div class="col-md-4 footer-grid footer_contact pull-right">
-								<h4>Get In Touch</h4>
-								<ul class="list catagories">
-		                            <li><i class="fa fa-envelope"></i>  info.petcareid@gmail.com</li>
-		                            <li><i class="fa fa-phone"></i> (021) 791 81 567</li>
-		                            <i class="fa fa-home"></i>
-									<span> Universitas Al Azhar Indonesia, lt.5 <br>
-									Kompleks Masjid Agung Al Azhar, Jl. Sisingamangaraja, Kebayoran Baru, RT.2/RW.1, Selong, Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12110</span>
-									</li>
-		                        </ul>
-								&nbsp
-		                        <h4>Jam Kerja</h4>
-		                            <p>Senin - Jum'at: 09.00am - 05.00pm <br>Minggu: <span>Tutup</span></p> 
-					</div> <!-- End of .footer_contact -->
+                        </div> <!-- End of .footer_logo -->
+					            <div class="col-md-4 footer-grid footer_contact pull-right">
+                                    <h4>Get In Touch</h4>
+                                    <ul class="list catagories">
+                                        <li><i class="fa fa-envelope"></i>  info.petcareid@gmail.com</li>
+                                        <li><i class="fa fa-phone"></i> (021) 791 81 567</li>
+                                        <i class="fa fa-home"></i>
+                                        <span> Universitas Al Azhar Indonesia, lt.5 <br>
+                                        Kompleks Masjid Agung Al Azhar, Jl. Sisingamangaraja, Kebayoran Baru, RT.2/RW.1, Selong, Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12110</span>
+                                        </li>
+                                    </ul>
+                                    &nbsp
+                                    <h4>Jam Kerja</h4>
+                                        <p>Senin - Jum'at: 09.00am - 05.00pm <br>Minggu: <span>Tutup</span></p>
+					            </div> <!-- End of .footer_contact -->
 				</div>
 				</div>
+                <br>
+            </div>
 	<!--//end-footer-section-->
 			<!--/start-copyright-section-->
 
